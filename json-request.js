faf9e7fd95b75762ef231d13bb9ec1a92a0be8f0
JoakimChan <120197963+JoakimChan@users.noreply.github.com>
2023-11-20 11:23:28 +0100

    commit

---
+++ json-request.js
@@ -0,0 +1,35 @@
+export async function getAllUsers() {
+  const res = await fetch('/data')
+  const data = await res.json()
+  return data
+}
+
+export async function addUser(name) {
+  let response = await fetch('/data', {
+    method: 'post',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      "name": name,
+      "matchHistory": []
+    })
+  })
+  response = await response.json()
+  console.log(response);
+}
+
+export async function getOneUser(userId) {
+  const res = await fetch('/data/' + userId)
+  const data = await res.json()
+  return data;
+}
+
+export async function updateUser(user) {
+  let response = await fetch('/data/' + user.id, {
+    method: 'put',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(user)
+  })
+
+  response = await response.json()
+  console.log(response);
+}
--- register.js
+++ register.js
@@ -1,65 +1,23 @@
-import fs from 'fs'
+import { getAllUsers, addUser, getOneUser, updateUser } from "./json-request.js"
+
 const playerArray = []
+const registerButton = document.getElementById('register')
 
-document.addEventListener('DOMContentLoaded', () => {
-  const registerButtom = document.getElementById('register')
-
-  registerButtom.addEventListener('click', register)
-})
+registerButton.addEventListener("click", register())
 
 function register() {
-  console.log('1')
-  fetchData()
   const newPlayerName = document.getElementById('player-name').value
-  console.log('3')
   if (checkForNameExist(newPlayerName)) {
     alert("player already existed")
   } else {
-    console.log('5')
-    const player = new NewPlayer(newPlayerName)
-
-    playerArray.push(player.dataInfo())
-    writeToJson();
-  }
-}
-
-function fetchData() {
-  console.log('2')
-  const jsonSting = fs.readFileSync("playerList.json");
-  const data = JSON.parse(jsonSting)
-
-  for (let i = 0; i < data.lengt; i++) {
-    playerArray.push(data[i])
+    addUser(newPlayerName)
   }
 }
 
 function checkForNameExist(name) {
-  console.log('4')
-  if (playerArray.some(x => x.playerName = name)) {
+  playerArray = getAllUsers()
+  if (playerArray.some(x => x.name = name)) {
     return true;
   }
   return false;
-}
-
-
-function writeToJson() {
-  fs.writeFileSync('./playerList.json', JSON.stringify(playerArray, null, 2), (err) => {
-    if (err) throw err;
-    console.log('data writen to file')
-  })
-}
-
-class NewPlayer {
-  constructor(name) {
-    this.name = name
-  }
-
-  dataInfo() {
-    return {
-      playerName: this.name,
-      won: 0,
-      loses: 0,
-      games: []
-    }
-  }
 }
